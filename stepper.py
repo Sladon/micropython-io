a9fdfa569d14947975a6a451d145ac51640e4545
Sebastian Allende <sallende@miuandes.cl>
2024-02-25 22:48:30 -0300

    refactor: simplify A4988 code

--- stepper.py
+++ stepper.py
@@ -75,125 +75,65 @@ class DriverDefault:
         for _ in range(steps):
             self.step()
 
-class DriverA4988:
-    
-    STEPS = 200
+class DriverA4988(DriverDefault):
 
-    STEP_MODES = {
-        "full": {"states": [0, 0, 0], "multiplier": 1},
-        "1/2":  {"states": [1, 0, 0], "multiplier": 2},
-        "1/4":  {"states": [0, 1, 0], "multiplier": 4},
-        "1/8":  {"states": [1, 1, 0], "multiplier": 8},
-        "1/16": {"states": [1, 1, 1], "multiplier": 16}
+    MICROSTEPS = {
+        1:  [0, 0, 0],
+        2:  [1, 0, 0],
+        4:  [0, 1, 0],
+        8:  [1, 1, 0],
+        16: [1, 1, 1],
     }
 
     def __init__(
-            self, direction: int or Pin, step: int or Pin, enable: int or Pin = None, ms1: int or Pin = None,
+            self, step: int or Pin, direction: int or Pin, enable: int or Pin = None, ms1: int or Pin = None,
             ms2: int or Pin = None, ms3: int or Pin = None, sleep: int or Pin = None, reset: int or Pin = None,
                 ):
-        
-        self.__mode = "full"
-        self.position = 0.0
-        self.__rpm = 0
 
-        self.dir_pin = self.__pin(direction)
-        self.step_pin = self.__pin(step)
+        super().__init__(step, direction, enable)
 
-        #* The following pins are not required to be connected to use the stepper motor
-        self.enable_pin = self.__pin(enable) if enable else None # If used, when LOW all FET outputs are enabled, off when HIGH
+        self.__enable_microstep = ms1 and ms2 and ms3
+        if self.__enable_microstep:
+            self.__ms1_pin = self.__pin(ms1)
+            self.__ms2_pin = self.__pin(ms2)
+            self.__ms3_pin = self.__pin(ms3)
 
-        self.__step_control = ms1 and ms2 and ms3
-        if self.__step_control:
-            self.ms1_pin = self.__pin(ms1)
-            self.ms2_pin = self.__pin(ms2)
-            self.ms3_pin = self.__pin(ms3)
-
-        #* If not needed, connect sleep with reset to enable the driver
-        self.sleep_pin = self.__pin(sleep) if sleep else None # If used, when LOW sleep mode enabled
-        self.reset_pin = self.__pin(reset) if reset else None # All step inputs are ignored if LOW
-        
-        self.sleep()
-        self.reset()
-        self.step_pin.value(0)
-        self.dir_pin.value(1)
-        self.enable()
-
-    def __pin(self, pin):
-        """
-        Adds a pin to the list of pins connected to the stepper motor.
-
-        Parameters:
-        - pin: Either an instance of the Pin class or an integer representing the GPIO pin number.
-        
-        Raises:
-        - TypeError: If the pin value is not of type 'int' or 'Pin'.
-        """
-        if isinstance(pin, Pin): return pin
-        elif isinstance(pin, int): return Pin(pin, Pin.OUT)
-        else: raise TypeError("pin value must be of type 'int' or 'Pin'")
-        
-    def step(self):
-        delay = self.delay()
-        prev_time_us = ticks_us()
-        times = 2
-        self.step_pin.value(0)
-        while True:
-            current_time_us = ticks_us()
-            if current_time_us - prev_time_us > delay:
-                self.step_pin.value(1)
-                prev_time_us = ticks_us()
-                break
-        
-    def enable(self):
-        if self.enable_pin is None: return "Pin not configured"
-        if self.enable_pin.value(): self.enable_pin.value(0)
-    
-    def disable(self):
-        if self.enable_pin is None: return "Pin not configured"
-        if not self.enable_pin.value(): self.enable_pin.value(1)
+        self.__sleep_pin = self.__pin(sleep) if sleep else None
+        self.__reset_pin = self.__pin(reset) if reset else None
     
     def sleep(self):
-        if self.sleep_pin is None: return "Pin not configured"
-        if self.sleep_pin.value(): self.sleep_pin.value(0)
+        if self.__sleep_pin is None: return "Pin not configured"
+        if self.__sleep_pin.value(): self.__sleep_pin.value(0)
     
     def awake(self):
-        if self.sleep_pin is None: return "Pin not configured"
-        if not self.sleep_pin.value():
+        if self.__sleep_pin is None: return "Pin not configured"
+        if not self.__sleep_pin.value():
             sleep_ms(1)
-            self.sleep_pin.value(1)
-    
+            self.__sleep_pin.value(1)
+
     def reset(self):
-        if self.reset_pin is None: return "Pin not configured"
-        self.reset_pin.value(0)
-        self.reset_pin.value(1)
+        if self.__reset_pin is None: return "Pin not configured"
+        self.__reset_pin.value(0)
+        self.__reset_pin.value(1)
         self.position = 0
+
+    def microsteps(self, microsteps: int = None):
+        if self.__enable_microstep and (microsteps is None or microsteps not in self.MICROSTEPS.keys()): return self.__microsteps
+        self.__microsteps = microsteps
+
+        ms1, ms2, ms3 = self.MICROSTEPS[microsteps]
+        self.__ms1_pin.value(ms1)
+        self.__ms2_pin.value(ms2)
+        self.__ms3_pin.value(ms3)
+            
+        return self.__microsteps
+
+    def rotate(self, steps):
+        orientation = 1 if self.direction() else -1
+        self.position += (1 / self.__microsteps * steps * orientation)
         
-    
-    def mode(self, mode: str = None):
-        if mode is not None:
-            ms1, ms2, ms3 = self.STEP_MODES[mode]["states"]
-            self.ms1_pin.value(ms1)
-            self.ms2_pin.value(ms2)
-            self.ms3_pin.value(ms3)
-            self.__mode = mode
-        return self.__mode
-    
-    def rpm(self, tg_rpm: float=None):
-        if tg_rpm is not None: self.__rpm = tg_rpm
-        return self.__rpm
-    
-    def delay(self, ):
-        return int(60*MICRO_TO_SECOND / (self.STEPS * self.STEP_MODES[self.__mode]["multiplier"] * self.__rpm))
-        
-    def rotate(self, steps, direction, force_mode: str = None):
-        self.dir_pin.value(direction)
-        orientation = 1 if direction else -1
-        self.position += (1 / self.STEP_MODES[self.__mode]["multiplier"]) * steps * orientation
-        
-        self.awake()
         for _ in range(steps):
             self.step()
-        self.sleep()
 
 class DriverTB6600(DriverDefault):
 
@@ -205,6 +145,8 @@ class DriverTB6600(DriverDefault):
     def microsteps(self, microsteps: int = None):
         if microsteps is None or microsteps not in self.MICROSTEPS: return self.__microsteps
         self.__microsteps = microsteps
-        return microsteps
+        return self.__microsteps
 
     # TODO: FIX Enable/Disable, probably hardware related
+
+motor = Driver2A4988(15,2,27,26,25,14,4,23)
